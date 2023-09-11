fe7f9e6a2d4e5155bc9fd0fa7a1e4dfdd829c2d3
Sean Feuerhelm <51465840+ParmaSeanCheese@users.noreply.github.com>
2023-09-11 11:47:48 -0500

    Add files via upload

---
+++ Big-Stompy-Robots/src/main/java/com/BigStompyRobots/BigStompyRobotsApplication.java
@@ -0,0 +1,13 @@
+package com.BigStompyRobots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigStompyRobotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigStompyRobotsApplication.class, args);
+	}
+
+}
